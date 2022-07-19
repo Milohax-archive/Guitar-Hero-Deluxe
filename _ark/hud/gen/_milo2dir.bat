8e90c00afdfb0b111be8eba3770c4a9cd0b22c4d
jnack <55568980+jnackmclain@users.noreply.github.com>
2022-07-18 19:06:00 -0500

    fixed multiplier at high res

--- _ark/hud/gen/_milo2dir.bat
+++ _ark/hud/gen/_milo2dir.bat
@@ -1,1 +1,1 @@
-FOR /F "tokens=*" %%G IN ('dir /b *.rnd_ps2') DO superfreq milo2dir "%%G" "_unpack\%%~nG" --convertTextures --miloVersion 10
+FOR /F "tokens=*" %%G IN ('dir /b *.rnd_ps2') DO superfreq milo2dir "%%G" "_unpack\%%~nG" --miloVersion 10
